18c6935a5d13df86a459bceb91db56b7dedd32dc
rohan-a99 <60211163+rohan-a99@users.noreply.github.com>
2022-01-11 12:46:29 +0530

    Update project_creation.sh

--- project_creation.sh
+++ project_creation.sh
@@ -1,4 +1,8 @@
 cd /
 cd workspaces/$1
+chmod +x workspaces/$1/load.sh
+chmod +x workspaces/$1/release.sh
+chmod +x workspaces/$1/build.sh
+chmod +x workspaces/$1/debug.sh
 git init
 git remote add origin $2
